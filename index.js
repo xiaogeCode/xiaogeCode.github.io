448e042d3469084d35a09b8e9c3668dc57c8daa2
xiaogeCode <30351354+xiaogeCode@users.noreply.github.com>
2022-07-04 02:46:46 +0800

    自定义标签页

---
+++ index.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', function name() {
+  let app = new Vue({
+    el: '#app',
+    data: {
+      name: 'hello',
+      sites:
+        [
+          ["baidu", "https://www.baidu.com", "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"],
+          ["bilibili", "https://www.bilibili.com", "https://i0.hdslb.com/bfs/archive/a115aa8c3a1604c3a639ec8679903432941624b5.png"],
+          ["虎扑", "https://bbs.hupu.com/all-gambia", "https://w1.hoopchina.com.cn/channel/website/static/images/basketball-nba-logo.png"],
+
+          ["观察者网", "https://www.guancha.cn/", "https://www.guancha.cn/images/mian-logo.png"],
+
+          ["google", "https://www.google.co.jp/", "#"],
+
+          ["anyknew", "https://www.anyknew.com/#/", "https://f0cdn.anyknew.com/static/anyknew-b.png"],
+
+          ["douyu", "https://www.douyu.com/", "#"],
+
+          ["nga", "https://bbs.nga.cn/thread.php?fid=650&rand=168", "https://img.nga.178.com/attachments/mon_202008/22/-10yuu8Q5-9lsxKeT8S2s-28.png"],
+
+          ["douyin", "https://www.douyin.com/?enter=guide&redirect=0", "#"],
+
+          ["youtube", "https://youtube.com/)", "#"],
+
+          ["逗比表情", "http://www.dbbqb.com", "http://resource.dbbqb.com/logo_black64.png"],
+
+          ["优酷", "https://youku.com/", "#"]
+        ]
+    },
+    methods: {},
+    components: {},
+    computed: {},
+    mounted() { }
+  })
+
+
+})
+
+
+
