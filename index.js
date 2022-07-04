89ac186a61ae50e5c8e626b686677ce66ca03f83
xiaoLoad <1065087424@qq.com>
2022-07-04 18:23:22 +0800

    更改网站Logo获取方法：直接在网址后+/favicon.ico

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ window.addEventListener('load', function name() {
 
           ["bilibili", "https://www.bilibili.com", "https://www.bilibili.com/favicon.ico"],
 
-          ["虎扑", "https://bbs.hupu.com/all-gambia", "https://bbs.hupu.com/favicon.ico"],
+          ["虎扑", "https://bbs.hupu.com", "https://bbs.hupu.com/favicon.ico"],
 
-          ["观察者", "https://www.guancha.cn/", "https://www.guancha.cn/favicon.ico"],
+          ["观察者", "https://www.guancha.cn", "https://www.guancha.cn/favicon.ico"],
 
-          ["google", "https://www.google.co.jp/", "#"],
+          ["google", "https://www.google.co.jp", "#"],
 
-          ["anyknew", "https://www.anyknew.com/", "https://f0cdn.anyknew.com/web/img/icons/favicon-32x32.png"],
+          ["anyknew", "https://www.anyknew.com", "https://f0cdn.anyknew.com/web/img/icons/favicon-32x32.png"],
 
-          ["douyu", "https://www.douyu.com/", "https://www.douyu.com/favicon.ico"],
+          ["douyu", "https://www.douyu.com", "https://www.douyu.com/favicon.ico"],
 
-          ["nga", "https://bbs.nga.cn/thread.php?fid=650&rand=168", "https://bbs.nga.cn/favicon.ico"],
+          ["nga", "https://bbs.nga.cn", "https://bbs.nga.cn/favicon.ico"],
 
-          ["douyin", "https://www.douyin.com/?enter=guide&redirect=0", "https://lf1-cdn-tos.bytegoofy.com/goofy/ies/douyin_web/public/favicon.ico"],
+          ["douyin", "https://www.douyin.com", "https://lf1-cdn-tos.bytegoofy.com/goofy/ies/douyin_web/public/favicon.ico"],
 
-          ["youtube", "https://youtube.com/)", "#"],
+          ["youtube", "https://youtube.com", "#"],
 
           ["逗比表情", "http://www.dbbqb.com", "http://www.dbbqb.com/logo192.png"],
 
-          ["优酷", "https://youku.com/", "https://img.alicdn.com/tfs/TB1WeJ9Xrj1gK0jSZFuXXcrHpXa-195-195.png"]
+          ["优酷", "https://youku.com", "https://img.alicdn.com/tfs/TB1WeJ9Xrj1gK0jSZFuXXcrHpXa-195-195.png"]
         ]
     },
     methods: {},
