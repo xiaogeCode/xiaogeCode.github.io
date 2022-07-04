caac26f9481535ce2eb9bb1723b8fde95c77f418
xiaoLoad <1065087424@qq.com>
2022-07-04 14:23:04 +0800

    更改loge获取网址

--- index.js
+++ index.js
@@ -5,27 +5,29 @@ window.addEventListener('load', function name() {
       name: 'hello',
       sites:
         [
-          ["baidu", "https://www.baidu.com", "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"],
-          ["bilibili", "https://www.bilibili.com", "https://i0.hdslb.com/bfs/archive/a115aa8c3a1604c3a639ec8679903432941624b5.png"],
-          ["虎扑", "https://bbs.hupu.com/all-gambia", "https://w1.hoopchina.com.cn/channel/website/static/images/basketball-nba-logo.png"],
+          ["baidu", "https://www.baidu.com", "https://www.baidu.com/favicon.ico"],
 
-          ["观察者网", "https://www.guancha.cn/", "https://www.guancha.cn/images/mian-logo.png"],
+          ["bilibili", "https://www.bilibili.com", "https://www.bilibili.com/favicon.ico"],
+
+          ["虎扑", "https://bbs.hupu.com/all-gambia", "http://w1.hoopchina.com.cn/images/pc/old/favicon.ico"],
+
+          ["观察者", "https://www.guancha.cn/", "https://i.guancha.cn/images/favorite.ico"],
 
           ["google", "https://www.google.co.jp/", "#"],
 
-          ["anyknew", "https://www.anyknew.com/#/", "https://f0cdn.anyknew.com/static/anyknew-b.png"],
+          ["anyknew", "https://www.anyknew.com/#/", "https://f0cdn.anyknew.com/web/img/icons/favicon-32x32.png"],
 
-          ["douyu", "https://www.douyu.com/", "#"],
+          ["douyu", "[https://www.douyu.com/", "https://www.douyu.com/favicon.ico"],
 
-          ["nga", "https://bbs.nga.cn/thread.php?fid=650&rand=168", "https://img.nga.178.com/attachments/mon_202008/22/-10yuu8Q5-9lsxKeT8S2s-28.png"],
+          ["nga", "https://bbs.nga.cn/thread.php?fid=650&rand=168", "https://bbs.nga.cn/favicon.ico"],
 
-          ["douyin", "https://www.douyin.com/?enter=guide&redirect=0", "#"],
+          ["douyin", "https://www.douyin.com/?enter=guide&redirect=0", "https://lf1-cdn-tos.bytegoofy.com/goofy/ies/douyin_web/public/favicon.ico"],
 
           ["youtube", "https://youtube.com/)", "#"],
 
-          ["逗比表情", "http://www.dbbqb.com", "http://resource.dbbqb.com/logo_black64.png"],
+          ["逗比表情", "http://www.dbbqb.com", "http://www.dbbqb.com/logo192.png"],
 
-          ["优酷", "https://youku.com/", "#"]
+          ["优酷", "https://youku.com/", "https://img.alicdn.com/tfs/TB1WeJ9Xrj1gK0jSZFuXXcrHpXa-195-195.png"]
         ]
     },
     methods: {},
