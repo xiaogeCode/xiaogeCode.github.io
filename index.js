160c7fd9672ea80c26c8d82a038a17936780ad0e
xiaoLoad <1065087424@qq.com>
2022-11-08 23:05:06 +0800

    更新显示方式为grid添加echart

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 window.addEventListener('load', function name() {
-  let app = new Vue({
-    el: '#app',
-    data: {
-      name: 'hello',
-      sites:
-        [
-          ["baidu", "https://www.baidu.com", "https://www.baidu.com/favicon.ico"],
 
-          ["bilibili", "https://www.bilibili.com", "https://www.bilibili.com/favicon.ico"],
+    let mapArea = 'china'
+    let app = new Vue({
+        el: '#app',
+        data: {
+            name: 'hello',
+            sites:
+                [
+                    ["baidu", "https://www.baidu.com", "https://www.baidu.com/favicon.ico"],
 
-          ["虎扑", "https://bbs.hupu.com", "https://bbs.hupu.com/favicon.ico"],
+                    ["bilibili", "https://www.bilibili.com", "https://www.bilibili.com/favicon.ico"],
 
-          ["观察者", "https://www.guancha.cn", "https://www.guancha.cn/favicon.ico"],
+                    ["虎扑", "https://bbs.hupu.com", "https://bbs.hupu.com/favicon.ico"],
 
-          ["google", "https://www.google.co.jp", "#"],
+                    ["观察者", "https://www.guancha.cn", "https://www.guancha.cn/favicon.ico"],
 
-          ["anyknew", "https://www.anyknew.com", "https://f0cdn.anyknew.com/web/img/icons/favicon-32x32.png"],
+                    ["google", "https://www.google.co.jp", "#"],
 
-          ["douyu", "https://www.douyu.com", "https://www.douyu.com/favicon.ico"],
+                    ["anyknew", "https://www.anyknew.com", "https://f0cdn.anyknew.com/web/img/icons/favicon-32x32.png"],
 
-          ["nga", "https://bbs.nga.cn", "https://bbs.nga.cn/favicon.ico"],
+                    ["douyu", "https://www.douyu.com", "https://www.douyu.com/favicon.ico"],
 
-          ["douyin", "https://www.douyin.com", "https://lf1-cdn-tos.bytegoofy.com/goofy/ies/douyin_web/public/favicon.ico"],
+                    ["nga", "https://bbs.nga.cn", "https://bbs.nga.cn/favicon.ico"],
 
-          ["youtube", "https://youtube.com", "#"],
+                    ["douyin", "https://www.douyin.com", "https://lf1-cdn-tos.bytegoofy.com/goofy/ies/douyin_web/public/favicon.ico"],
 
-          ["逗比表情", "http://www.dbbqb.com", "http://www.dbbqb.com/logo192.png"],
+                    ["youtube", "https://youtube.com", "#"],
 
-          ["优酷", "https://youku.com", "https://img.alicdn.com/tfs/TB1WeJ9Xrj1gK0jSZFuXXcrHpXa-195-195.png"]
-        ]
-    },
-    methods: {},
-    components: {},
-    computed: {},
-    mounted() { }
-  })
+                    ["逗比表情", "http://www.dbbqb.com", "http://www.dbbqb.com/logo192.png"],
+
+                    ["优酷", "https://youku.com", "https://img.alicdn.com/tfs/TB1WeJ9Xrj1gK0jSZFuXXcrHpXa-195-195.png"]
+                ]
+        },
+        methods: {},
+        components: {},
+        computed: {},
+        mounted() { }
+    });
+    var chart = echarts.init(document.getElementById("map"));
+    chart.setOption({
+        series: [{
+            "type": "map",
+            "map": mapArea
+        }]
+    });
 
 
 })
